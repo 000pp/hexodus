69a96f69f2dedc1c814fc8171bf7f8c1da3ff42c
opps3c <opps3c@gmail.com>
2025-07-25 17:54:07 -0300

    improvement: added paged_search to handle more than 1000 users

--- src/modules/ldap/users.py
+++ src/modules/ldap/users.py
@@ -10,25 +10,44 @@ class Users:
 
     def on_login(self, conn, base_dn, save_output = False, module_args = None):
         console.print(f"[[green]+[/]] [cyan]MODULE[/]  Running [yellow]{self.name}[/] module")
-
-        results = conn.search(base_dn, self.search_filter, attributes=self.attributes)
-        entries = conn.entries
-
-        if not entries:
-            console.print(f"\n[[red]![/]] No results for {self.name}") 
-            return
-        
         console.print(f"[[green]+[/]] [cyan]QUERY[/]   [white]{self.search_filter}[/]\n", highlight=False)
 
-        values = [entry[self.attributes].value for entry in conn.entries]
-        for value in values:
-            console.print(value, highlight=False)
+        try:
+            generator = conn.extend.standard.paged_search(
+                search_base=base_dn,
+                search_filter=self.search_filter,
+                search_scope='SUBTREE',
+                attributes=[self.attributes],
+                paged_size=1000,
+                generator=True
+            )
 
-        if save_output:
-            filename = f"{self.name}_{uuid4().hex}.txt"
-            with open(filename, "w", encoding="utf-8") as f:
-                for v in values:
-                    f.write(v + "\n")
-            console.print(f"\n[[green]+[/]] Output saved to {filename}", highlight=False)
+            values = []
+            for entry in generator:
+                if entry.get('type') != 'searchResEntry':
+                    continue
 
-        return values
+                username_attribute = entry.get('attributes', {}).get(self.attributes)
+                if not username_attribute:
+                    continue
+
+                value = username_attribute[0] if isinstance(username_attribute, list) else username_attribute
+                values.append(value)
+                console.print(value, highlight=False)
+            
+            if not values:
+                console.print(f"\n[[red]![/]] No results for {self.name}") 
+                return
+            
+            if save_output:
+                filename = f"{self.name}_{uuid4().hex}.txt"
+                with open(filename, "w", encoding="utf-8") as f:
+                    for v in values:
+                        f.write(v + "\n")
+                console.print(f"\n[[green]+[/]] Output saved to {filename}", highlight=False)
+
+            return values
+        
+        except Exception as error:
+            console.print(f"[[red]![/]] Error in users.py module: {error}", highlight=False)
+            raise
